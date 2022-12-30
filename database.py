0cb2ff6ca359e26cc8f71e5f1dee3931b31e3d94
erroronline1 <dev@erroronline.one>
2022-12-31 00:25:56 +0100

    conditional string sanitation and returning latest primary key

--- database.py
+++ database.py
@@ -2,9 +2,10 @@ import sqlite3
 
 class DataBase():
 	tableFields = {
-		"CS": ["ID", "DATE", "RATING0", "RATING1", "RATING2", "RATING3", "COMMENDATION", "SUGGESTION", "SERVICE"],
+		"CS": ["ID", "DATE", "RATING", "RATING0", "RATING1", "RATING2", "RATING3", "COMMENDATION", "SUGGESTION", "SERVICE"],
 		"SETTING": ["KEY", "VALUE"]
 	}
+	password = ''
 	def __init__(self, db):
 		try:
 			self.connection = sqlite3.connect(db)
@@ -14,7 +15,7 @@ class DataBase():
 				self.create()
 			else:
 				getpwd = self.read("SETTING", {"KEY":"password"})
-				self.password = getpwd[0][1] if getpwd else None
+				self.password = getpwd[0][1] if getpwd else ''
 		except Exception as error:
 			pass
 
@@ -25,14 +26,15 @@ class DataBase():
 		self.connection.executescript(f'''
 			CREATE TABLE CS
 			({self.tableFields["CS"][0]} INTEGER PRIMARY KEY AUTOINCREMENT,
-			{self.tableFields["CS"][1]} TINYTEXT NOT NULL,
+			{self.tableFields["CS"][1]} TINYTEXT,
 			{self.tableFields["CS"][2]} INTEGER,
 			{self.tableFields["CS"][3]} INTEGER,
 			{self.tableFields["CS"][4]} INTEGER,
 			{self.tableFields["CS"][5]} INTEGER,
-			{self.tableFields["CS"][6]} TEXT,
+			{self.tableFields["CS"][6]} INTEGER,
 			{self.tableFields["CS"][7]} TEXT,
-			{self.tableFields["CS"][8]} TINYTEXT);
+			{self.tableFields["CS"][8]} TEXT,
+			{self.tableFields["CS"][9]} TINYTEXT);
 
 			CREATE TABLE SETTING
 			({self.tableFields["SETTING"][0]} TINYTEXT UNIQUE,
@@ -43,27 +45,32 @@ class DataBase():
 	def write(self, table="", key_value={}, where={}):
 		condition, values, updates = [], [], []
 		for key in where:
-			condition.append(f"{self.sanitize(key)}='{self.sanitize(where[key])}'")
+			condition.append(f"{self.sanitize(key, False)}={self.sanitize(where[key])}")
 		for column in self.tableFields[table]:
 			if column in key_value:
-				values.append(f"'{self.sanitize(key_value[column])}'")
-				updates.append(f"{column}='{self.sanitize(key_value[column])}'")
+				values.append(f"{self.sanitize(key_value[column])}")
+				updates.append(f"{column}={self.sanitize(key_value[column])}")
 			else:
-				values.append("''")
+				values.append("NULL")
 		# try to update exsting
 		if len(condition):
 			self.connection.execute(f"UPDATE {table} SET {', '.join(updates)} WHERE {' AND '.join(condition)};")
-			print(f"UPDATE {table} SET {', '.join(updates)} WHERE {' AND '.join(condition)};")
 		# otherwise insert
 		self.connection.execute(f"INSERT OR IGNORE INTO {table} ({', '.join(self.tableFields[table])}) VALUES ({', '.join(values)});")
-		print(table, key_value, where, f"INSERT OR IGNORE INTO {table} ({', '.join(self.tableFields[table])}) VALUES ({', '.join(values)});")
 		self.connection.commit()
+
+		if table=="CS":
+			cursor = self.connection.cursor()
+			cursor.execute(f"SELECT MAX(ID) FROM CS;")
+			result = cursor.fetchone()
+			if result is not None:
+				return result[0]
 		return True
 
 	def read(self, table="", where={}, explicit=[]):
 		condition, explicit = [], [self.sanitize(field) for field in explicit] if explicit else "*"
 		for key in where:
-			condition.append(f"{self.sanitize(key)}='{self.sanitize(where[key])}'")
+			condition.append(f"{self.sanitize(key, False)}={self.sanitize(where[key])}")
 		cursor = self.connection.cursor()
 		cursor.execute(f"SELECT {' AND '.join(explicit)} FROM {table} WHERE {' AND '.join(condition)};")
 		result = cursor.fetchall()
@@ -71,8 +78,10 @@ class DataBase():
 			return result
 		return False
 
-	def sanitize(self, string=""):
-		return string.replace('\'','\'\'')
+	def sanitize(self, value="", quotes = True):
+		if type(value)==str and value != "NULL":
+			return value.replace('\'','\'\'') if not quotes else "'" + value.replace('\'','\'\'') + "'"
+		return value
 
 	def clear(self, tables=[]):
 		for table in tables:
